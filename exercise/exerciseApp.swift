bb8932a98ff49d1bed2f0accc0b72a236b79d3d0
Fajar Rashid <fajarrashid786@gmail.com>
2024-07-12 16:04:51 -0400

    Initial Commit

---
+++ exercise/exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  exerciseApp.swift
+//  exercise
+//
+//  Created by Fajar Rashid on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
